c39948297b49afba7ec30ec3d207f3f2404ac258
DhulipallaSaiTeja <158553693+DhulipallaSaiTeja@users.noreply.github.com>
2024-02-08 12:03:25 +0530

    ifElse

---
+++ ifElse.js
@@ -0,0 +1,60 @@
+// ifElse
+
+const ageLimitForMovie = 22;
+if (ageLimitForMovie <= 17){
+    console.log("No age Limit")
+} else {
+    console.log("there was a age restriction")
+}
+
+const ageLimitForMovies = 22;
+if (ageLimitForMovies <= 65){
+    console.log("No age Limit")
+} else {
+    console.log("there was a age restriction")
+}
+
+const age = 22;
+if (age === 22) {
+    console.log("age is correct")
+} else {
+    console.log("age is not correct")
+}
+
+const ages = "22";
+if (ages === 22) {
+    console.log("age is correct")
+} else {
+    console.log("age is not correct")
+}
+
+let time = 18
+if (time <= 18){
+    console.log("Have a good Day")
+} else {
+    console.log("Have a Good Evening")
+}
+
+if (time <= 19){
+    console.log("Have a good Day")
+} else {
+    console.log("Have a Good Evening")
+}
+
+if (0){
+    console.log("Have a good Day")
+} else {
+    console.log("Have a Good Evening")
+}
+
+if (null){
+    console.log("Have a good Day")
+} else {
+    console.log("Have a Good Evening")
+}
+
+if (undefined){
+    console.log("Have a good Day")
+} else {
+    console.log("Have a Good Evening")
+}
