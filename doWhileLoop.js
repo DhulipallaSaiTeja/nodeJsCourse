21452d4a4a90bef15f0d8ea07d3118d251871813
DhulipallaSaiTeja <158553693+DhulipallaSaiTeja@users.noreply.github.com>
2024-02-14 17:52:10 +0530

    doWhileLoop

---
+++ doWhileLoop.js
@@ -0,0 +1,46 @@
+// doWhileLoop
+
+let i = 1; //intialization
+do{
+    console.log(i) //statement
+    i++             // increment
+}
+while(i<=20)    //condition
+
+
+let a = 0;
+do{
+    console.log(a)
+    a++
+}
+while(a<=10)
+
+let b = 15;
+do{
+    console.log(b)
+    b--
+}
+while(b>=10)
+
+
+let c = 1;
+do{
+    if(c % 2 == 0){
+    console.log(c)
+    }
+    c++
+}
+while(c<=10)
+
+
+let d = 10;
+do{
+    if(d % 2 !== 0){
+        console.log(d)
+    }
+    d++
+}
+while(d<=20)
+
+
+
