e47cb6eb28befef9f9d7d81207cc14dff265fe24
DhulipallaSaiTeja <158553693+DhulipallaSaiTeja@users.noreply.github.com>
2024-02-20 17:55:33 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,80 @@
+// Palindrome
+
+const a = `eat`;
+const b = a.split(``)
+const c = b.reverse(``)
+const d = c.join(``)
+if(a == d)
+{
+    console.log(`It's a Palindrome`)
+}
+else
+{
+    console.log(`It's a not Palindrome`)
+}
+
+const e = 'racecar'
+const f = e.split(``)
+const g = f.reverse(``)
+const i = g.join(``)
+if(e == i)
+{
+    console.log("Racecar is a palindrome")
+}
+else
+{
+    console.log("Racecar is not a palindrome")
+}
+
+const j = "MADAM"
+const k = j.split(``)
+const l = k.reverse(``)
+const m = l.join(``)
+if (j == m)
+{
+    console.log("MADAM is a palindrome")
+}
+else
+{
+    console.log("MADAM is not a palindrome")
+}
+
+const n = `civic`
+const o = n.split(``)
+const p = o.reverse(``)
+const q = p.join(``)
+if (n == q)
+{
+    console.log("civic is a palindrome")
+}
+else
+{
+    console.log("civic is not a palindrome")
+}
+
+const r = `great`
+const s = r.split(``)
+const t = s.reverse(``)
+const u = t.join(``)
+if (r == u)
+{
+    console.log("great is a palindrome")
+}
+else
+{
+    console.log("great is not a palindrome")
+}
+
+const v = `A Toyota's a Toyota`
+const w = v.split(``)
+const x = w.reverse(``)
+const y = x.join(``)
+if (v == y)
+{
+    console.log(`A Toyota's a Toyota is a palindrome`)
+}
+else
+{
+    console.log(`A Toyota's a Toyota is not a palindrome`)
+
+}
